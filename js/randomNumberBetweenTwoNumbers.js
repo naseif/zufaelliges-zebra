4e76513201201b4e940da09d0d22896096549c28
Naseif <d2mds21@gmail.com>
2020-07-24 13:04:33 +0200

    Added a welcome alert

--- js/randomNumberBetweenTwoNumbers.js
+++ js/randomNumberBetweenTwoNumbers.js
@@ -3,40 +3,44 @@
 
 function generator() {
   vex.dialog.prompt({
-  message: 'Please provide your lowest Number?',
-  placeholder: 'Type a Number',
-  callback: function (value) {
+    message: 'Please provide your lowest Number?',
+    placeholder: 'Type a Number',
+    callback: function (value) {
 
-    const valueconv = parseInt(value)  // Converted the value string into a number
-    
+      const valueconv = parseInt(value)  // Converted the value string into a number
+
       vex.dialog.prompt({
         message: 'Please provide your highest Number?',
         placeholder: 'Type a Number',
         callback: function (value2) {
-        
+
           const value2conv = parseInt(value2)  // // Converted the value string into a number
 
-         if (valueconv < value2conv) {
+          if (valueconv < value2conv) {
 
-            const randomNumber = Math.floor( Math.random() * (value2conv - valueconv + 1) ) + valueconv;
+            const randomNumber = Math.floor(Math.random() * (value2conv - valueconv + 1)) + valueconv;
             vex.dialog.alert(`The random number between ${valueconv} and ${value2conv} is ${randomNumber}`);
-        
-        } else if (valueconv === value2conv) {
-          
-          vex.dialog.alert("You Provided the same Number for both fields!. Please type different Numbers!");
-        
-        } else if (valueconv > value2conv) {
-        
-          vex.dialog.alert("You provided a higher low number! Please provide a low and high numbers correctly!");
-        } else {
-        
-          vex.dialog.alert("You provided a wrong Number! Please try again.");
-        }
 
+          } else if (valueconv === value2conv) {
+
+            vex.dialog.alert("You Provided the same Number for both fields!. Please type different Numbers!");
+
+          } else if (valueconv > value2conv) {
+
+            vex.dialog.alert("You provided a higher low number! Please provide a low and high numbers correctly!");
+          } else {
+
+            vex.dialog.alert("You provided a wrong Number! Please try again.");
           }
-        })
-  }
-})
+        }
+      });
+    }
+  });
 }
 
-generator();
+vex.dialog.alert({
+  message: "Welcome to the Random Number Generator",
+  callback: generator
+});
+
+
