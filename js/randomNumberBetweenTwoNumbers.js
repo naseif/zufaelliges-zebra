703241ebc496d2b8d4e608b873f912405e0d6011
naseif <d2mds21@gmail.com>
2021-03-20 19:41:43 +0100

    Refactored the whole code with my buddy Stefan

--- js/randomNumberBetweenTwoNumbers.js
+++ js/randomNumberBetweenTwoNumbers.js
@@ -1,47 +1,11 @@
-// A Simple Random Number Generator
+var generator = RandomNumberGenerator();
 
-function generator() {
-  vex.dialog.prompt({
-    message: 'Please provide your lowest Number?',
-    placeholder: 'Type a Number',
-    callback: function (value) {
+var uiWorkflow = [
+  { type: "alert", message: "Welcome to the Random Number Generator" },
+  { type: "prompt",  message: "Please provide your lowest number!",  placeholder: "type a number", call : generator.lowestNumberEntered  },
+  { type: "prompt",  message: "Please provide your highest number!", placeholder: "type a number", call : generator.highestNumberEntered },
+  { type: "alert", message: generator.evaluation }
+];
 
-      const valueconv = parseInt(value)  // Converted the value string into a number
-
-      vex.dialog.prompt({
-        message: 'Please provide your highest Number?',
-        placeholder: 'Type a Number',
-        callback: function (value2) {
-
-          const value2conv = parseInt(value2)  // // Converted the value string into a number
-
-          if (valueconv < value2conv) {
-
-            const randomNumber = Math.floor(Math.random() * (value2conv - valueconv + 1)) + valueconv;
-            vex.dialog.alert(`The random number between ${valueconv} and ${value2conv} is ${randomNumber}`);
-
-          } else if (valueconv === value2conv) {
-
-            vex.dialog.alert("You Provided the same Number for both fields!. Please type different Numbers!");
-
-          } else if (valueconv > value2conv) {
-
-            vex.dialog.alert("You provided a higher low number! Please provide a low and high numbers correctly!");
-          } else if (isNaN(valueconv) || isNaN(valueconv2)) {
-
-            vex.dialog.alert("Both fields should be provided with numbers.Please try again.");
-          } else {
-
-            vex.dialog.alert("You entered a false number or value. Please try again.");
-
-          }
-        }
-      });
-    }
-  });
-}
-
-vex.dialog.alert({
-  message: "Welcome to the Random Number Generator",
-  callback: generator
-});
+var app = UiWorkflowProcessor(uiWorkflow);
+app.run();
