c0af1a43d01bcfb870e6dd9fbd7406dd30cb1349
naseif <d2mds21@gmail.com>
2021-06-06 13:24:56 +0000

    Deleted unnecessary lines

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-// document.getElementById("randomNumber").addEventListener("click", ExecuteRandomNumberGenerator);
-// document.getElementById("randomPassword").addEventListener("click", function() {
-//     const alleMoeglichenZeichen = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?!$";
-//     const maximalLength = 18;
-//     let result = "";
-
-//     for (let i = 0; i < maximalLength; i++) {
-//         const randomNumber = Math.floor(Math.random() * alleMoeglichenZeichen.length)
-//         result += alleMoeglichenZeichen[randomNumber]
-
-//     }
-//     navigator.clipboard.writeText(result);
-//     vex.dialog.alert('Password has been copied to your clipboard')
-// })
-
-// $(document).ready(function(){
-//    let randomPasswords = GenerateABunchOfRandomPasswords(10);
-//     $("#passwordArea").val(randomPasswords);
-// });
-
 function GeneratePasswordsInUi(){
     const passwordlength = Number($("#passwordlength").val());
     $("#passwordArea").val(GenerateABunchOfRandomPasswords(10, passwordlength));
