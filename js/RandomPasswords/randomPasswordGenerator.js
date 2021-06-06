858874a50deef30fae286b20dd5d228c8f1ba61e
naseif <d2mds21@gmail.com>
2021-06-06 13:17:35 +0000

    Refactored the website. Added generate random password based on user input. Added funny animal name generator to the ui (niy)

---
+++ js/RandomPasswords/randomPasswordGenerator.js
@@ -0,0 +1,28 @@
+/**
+ * Generates random passwords.
+ * These are like passwords - only random.
+ * Thats amazing and adds a lot of security, unless you forget them, which is not a good idea.
+ * Anyways, I forgot what I wanted to type ... and say ... amum aah, ok, line 7..
+ */
+function RandomPasswordGenerator(passwordlength){
+    const validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?!$";
+    const maximalLength = passwordlength;
+    let result = "";
+
+    for (let i = 0; i < maximalLength; i++) {
+        const randomNumber = Math.floor(Math.random() * validCharacters.length)
+        result += validCharacters[randomNumber]
+    }
+
+    return result;
+} 
+
+function GenerateABunchOfRandomPasswords(howMany, passwordlength){
+    let result = '';
+
+    for (let i = 0; i < howMany; i++){
+        result += RandomPasswordGenerator(passwordlength) + "\n";
+    } 
+
+    return result.trim();
+}
