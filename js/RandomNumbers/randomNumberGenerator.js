67d49efee2c57feb62b450911e7c305d5a7be101
naseif <d2mds21@gmail.com>
2021-06-08 09:20:21 +0200

    Cleaned the repository from unnecessary code and libraries

--- js/RandomNumbers/randomNumberGenerator.js
+++ js/RandomNumbers/randomNumberGenerator.js
@@ -1,48 +1,7 @@
-// /***
-//  * RandomNumberGenerator - Generiert Zufallszahlen basierend auf naja, dem Üblichen
-//  *
-//  */
-// function RandomNumberGenerator() {
-//   let api = {};
-
-//   api.lowestNumber = 0;
-//   api.highestNumber = 0;
-
-//   api.lowestNumberEntered = function (value) {
-//     api.lowestNumber = parseInt(value);
-//   };
-
-//   api.highestNumberEntered = function (value) {
-//     api.highestNumber = parseInt(value);
-//   };
-
-//   api.evaluation = function () {
-//     let lowestNum = api.lowestNumber;
-//     let highestNum = api.highestNumber;
-
-//     if (lowestNum < highestNum) {
-//       const randomNumber =
-//         Math.floor(Math.random() * (highestNum - lowestNum + 1)) + lowestNum;
-//       return `The random number between ${lowestNum} and ${highestNum} is ${randomNumber}`;
-//     }
-
-//     if (lowestNum === highestNum) {
-//       return "You Provided the same Number for both fields!. Please type different Numbers!";
-//     }
-
-//     if (lowestNum > highestNum) {
-//       return "You provided a higher low number! Please provide a low and high numbers correctly!";
-//     }
-
-//     if (isNaN(lowestNum) || isNaN(highestNum)) {
-//       return "Both fields should be provided with numbers.Please try again.";
-//     }
-
-//     return "You entered a false number or value. Please try again.";
-//   };
-
-//   return api;
-// }
+/***
+ * RandomNumberGenerator - Generiert Zufallszahlen basierend auf naja, dem Üblichen
+ *
+ */
 
 function GenerateRandomNumbersInUi() {
   let lowestNum = Number($("#lowestNumber").val());
