d4572d137f36bdba4fe1cf044bc0f9dbc884377f
stho32 <stho32github@gmail.com>
2022-02-27 19:17:49 +0100

    add random animal generator and tests

--- source/lib/RandomAnimals/RandomAnimalGenerator.spec.ts
+++ source/lib/RandomAnimals/RandomAnimalGenerator.spec.ts
@@ -1,5 +1,27 @@
 import expect from "expect";
+import { RandomAnimalGenerator } from "./RandomAnimalGenerator";
 
 describe("RandomAnimalGenerator", () => {
-  describe("when called", () => {});
+  describe("when initialized with one option", () => {
+    it("returns that option", () => {
+      const randomAnimalGenerator = 
+        new RandomAnimalGenerator(["tiger"], (min,max) => 0);
+
+      const result = randomAnimalGenerator.generate();
+
+      expect(result).toBe("tiger");
+    });
+  });
+
+  describe("when the random number generator selects and option that is not part of the animals collection", () => {
+    it("throws an error", () => {
+      const randomAnimalGenerator = 
+        new RandomAnimalGenerator(["tiger"], (min,max) => 1);
+
+      expect(() => { randomAnimalGenerator.generate() }).toThrowError();
+    });
+  });
 });
+
+
+
