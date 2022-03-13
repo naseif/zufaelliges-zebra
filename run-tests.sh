0b045db74c5f5d61309bd4eeb5579681c660b905
stho32 <stho32github@gmail.com>
2022-03-13 17:48:58 +0100

    build: changed name of library, so that our frontend can have a main.js without creating confusion

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf dist/
-npx tsup source-backend/main.ts --dts-resolve
+npx tsup source-backend/zufaelliges-zebra.bl.ts --dts-resolve
 npx nyc npx ts-mocha -p ./tsconfig.json source-backend/**/*.spec.ts
